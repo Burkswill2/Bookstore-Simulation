2676ba77d76f85bb9545f2f70045488dd2a8e3d7
Will Burks <burks.113@osu.edu>
2024-04-02 19:08:41 -0400

    Revised

--- lab2.c
+++ lab2.c
@@ -1,11 +1,8 @@
 // DO NOT REMOVE THIS COMMENT!! CSE 3430 lab2.c SP 24 CODE 082108
-// STUDENT NAME: (REPLACE THIS WITH YOUR NAME)
-
+// STUDENT NAME: William Burks
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// Structure for storing book data
 struct Data {
     char title[60];
     char author[60];
@@ -15,14 +12,10 @@ struct Data {
     int wholesaleQuantity;
     int retailQuantity;
 };
-
-// Node structure for the linked list
 typedef struct Node {
     struct Data book;
     struct Node *next;
 } Node;
-
-// Function prototypes
 void getDataAndBuildList(Node **listHeadPtr);
 Node *createNodeAndGetData(void);
 void insertNode(Node **listHeadPtr, Node *newNodePtr);
@@ -43,7 +36,6 @@ int main(void) {
     getUserOption(&listHead);
     return 0;
 }
-
 void getDataAndBuildList(Node **listHeadPtr) {
     Node *newNodePtr;
     printf("Please enter data about the books.\n\n");
@@ -51,20 +43,24 @@ void getDataAndBuildList(Node **listHeadPtr) {
         insertNode(listHeadPtr, newNodePtr);
     }
 }
-
 Node *createNodeAndGetData(void) {
-    Node *newNodePtr = malloc(sizeof(Node));
+    Node *newNodePtr;
+    newNodePtr = malloc (sizeof(Node));
     if (newNodePtr == NULL) {
         printf("Error: memory could not be allocated for enough nodes. ");
         printf("Terminating program!\n");
-        exit(0);
-    } else {
+        exit (0);
+    }
+    else {
         scanf("%[^\n]", newNodePtr->book.title);
         if (strcmp(newNodePtr->book.title, "END_DATA") == 0) {
-            free(newNodePtr); // Free node if end of book data detected
+/* free Node if end of book data detected */
+            free(newNodePtr);
             return NULL;
-        } else {
-            getchar(); // Consume newline before author string
+        }
+        else {
+/* consume newline before author string */
+            getchar();
             scanf("%[^\n]s", newNodePtr->book.author);
             scanf("%d", &newNodePtr->book.stockNumber);
             scanf("%f", &newNodePtr->book.wholesalePrice);
@@ -78,65 +74,76 @@ Node *createNodeAndGetData(void) {
 }
 
 void insertNode(Node **listHeadPtr, Node *newNodePtr) {
-    // If list is empty or if the new node should be placed before the head
-    if (*listHeadPtr == NULL || (*listHeadPtr)->book.stockNumber > newNodePtr->book.stockNumber) {
-        newNodePtr->next = *listHeadPtr; // Point the new node to the current head
-        *listHeadPtr = newNodePtr; // Update the head to be the new node
-    } else {
-        // Search for the correct position to insert the new node
-        Node *current = *listHeadPtr;
-        while (current->next != NULL && current->next->book.stockNumber < newNodePtr->book.stockNumber) {
-            current = current->next; // Move to the next node
+    Node *traversePtr = *listHeadPtr;
+    // Empty list case: insert first Node
+    if (traversePtr == NULL) {
+        *listHeadPtr = newNodePtr;
+        newNodePtr->next = NULL;
+    }
+        // Non-empty list: insert new 1st Node before current 1st Node
+    else if (newNodePtr->book.stockNumber < traversePtr->book.stockNumber) {
+        *listHeadPtr = newNodePtr;
+        newNodePtr->next = traversePtr;
+    }
+        // Non-empty list case: insert new Node after current 1st Node
+    else {
+        Node *priorNodePtr = traversePtr;
+        traversePtr = traversePtr->next;
+        while (traversePtr != NULL && traversePtr->book.stockNumber < newNodePtr->book.stockNumber) {
+            // Move each pointer to next Node
+            priorNodePtr = traversePtr;
+            traversePtr = traversePtr->next;
         }
-        // Insert the new node
-        newNodePtr->next = current->next;
-        current->next = newNodePtr;
+        // Change pointers to add newNodePtr Node to list
+        priorNodePtr->next = newNodePtr;
+        newNodePtr->next = traversePtr;
     }
 }
 
 void deleteNode(Node **listHeadPtr, int stockNumToDelete) {
-    Node *temp = *listHeadPtr, *prev = NULL;
-
-    // If head node itself holds the stock number to be deleted
-    if (temp != NULL && temp->book.stockNumber == stockNumToDelete) {
-        *listHeadPtr = temp->next; // Changed head
-        free(temp); // free old head
-        printf("Book stock number %d deleted from the inventory.\n", stockNumToDelete);
-        return;
+    Node *traversePtr = *listHeadPtr;
+    // Empty list case: ERROR
+    if (traversePtr == NULL) {
+        printf("ERROR: The list is empty; there are no Nodes to delete!\n");
     }
-
-    // Search for the stock number to be deleted, keep track of the previous node
-    while (temp != NULL && temp->book.stockNumber != stockNumToDelete) {
-        prev = temp;
-        temp = temp->next;
+        // Non-empty list: delete 1st Node
+    else if (traversePtr->book.stockNumber == stockNumToDelete) {
+        *listHeadPtr = traversePtr->next;
+        free(traversePtr);
+        printf("Book stock number %d deleted!\n", stockNumToDelete);
     }
-
-    // If stock number was not present in linked list
-    if (temp == NULL) {
-        printf("Book stock number %d not found in the inventory.\n", stockNumToDelete);
-        return;
+        // Non-empty list case: delete Node after current 1st Node
+    else {
+        Node *priorNodePtr = traversePtr;
+        traversePtr = traversePtr->next;
+        while (traversePtr != NULL && traversePtr->book.stockNumber < stockNumToDelete) {
+            // Move each pointer to next Node
+            priorNodePtr = traversePtr;
+            traversePtr = traversePtr->next;
+        }
+        // Check for ERROR; change pointers to delete Node
+        if (traversePtr == NULL || traversePtr->book.stockNumber != stockNumToDelete) {
+            printf("ERROR: Book stock number %d not found in list!\n", stockNumToDelete);
+        }
+        else {
+            priorNodePtr->next = traversePtr->next;
+            free(traversePtr);
+            printf("Book stock number %d deleted!\n", stockNumToDelete);
+        }
     }
-
-    // Unlink the node from linked list
-    prev->next = temp->next;
-
-    free(temp); // Free memory
-    printf("Book stock number %d deleted from the inventory.\n", stockNumToDelete);
 }
-
-
 
 void getUserOption(Node **listHeadPtr) {
     int option;
     Node *newNodePtr;
     int stockNumToDelete;
     do {
-        printf("\nPlease enter an integer between 1 and 10 to select an operation on the data:\n");
-        scanf("%d", &option);
-        getchar(); // Consume newline
-        switch (option) {
+        printf("\nPlease enter an integer between 1 and 10 to select an operation on the data: ");
+        scanf("%i", &option);
+        getchar();
+        switch (option){
             case 1:
-                printList(*listHeadPtr);
+                printList (*listHeadPtr);
                 break;
             case 2:
                 printf("\nTotal revenue: %.2f\n", calculateTotalRevenue(*listHeadPtr));
@@ -145,42 +152,37 @@ void getUserOption(Node **listHeadPtr) {
                 printf("\nTotal wholesale cost: %.2f\n", calculateTotalWholesaleCost(*listHeadPtr));
                 break;
             case 4:
-                printf("\nInvestment in inventory: %.2f\n", calculateInvestmentInInventory(*listHeadPtr));
+                printf("\nTotal investment in inventory: %.2f\n",
+                       calculateInvestmentInInventory(*listHeadPtr));
                 break;
             case 5:
                 printf("\nTotal profit: %.2f\n", calculateTotalProfit(*listHeadPtr));
                 break;
             case 6:
-                printf("\nTotal number of books sold: %d\n", calculateTotalBooksSold(*listHeadPtr));
+                printf("\nTotal number of books sold = %d\n", calculateTotalBooksSold(*listHeadPtr));
                 break;
             case 7:
-                printf("\nAverage profit per sale: %.2f\n", calculateAverageProfit(*listHeadPtr));
+                printf("\nAverage profit: %.2f\n", calculateAverageProfit(*listHeadPtr));
                 break;
             case 8:
-                {
-                    Node *newNode = createNodeAndGetData();
-                    if (newNode) {
-                        insertNode(listHeadPtr, newNode);
-                        printf("New book added.\n");
-                    }
-                }
+                printf("\nPlease enter the data for the book you wish to add:\n\n");
+                newNodePtr = createNodeAndGetData();
+                insertNode(listHeadPtr, newNodePtr);
                 break;
             case 9:
-                printf("Enter stock number of the book to delete: ");
+                printf("\nEnter the stock number of the book you wish to delete: ");
                 scanf("%d", &stockNumToDelete);
                 deleteNode(listHeadPtr, stockNumToDelete);
                 break;
             case 10:
-                printf("Exiting program...\n");
                 freeAllNodes(listHeadPtr);
                 break;
             default:
-                printf("Invalid option. Please enter a number between 1 and 10.\n");
+                printf("Valid option choices are 1 to 10. Please choose again!\n");
+                break;
         }
     } while (option != 10);
 }
-
-// Placeholder for functions calculateTotalRevenue, calculateInvestmentInInventory, etc.
 double calculateTotalRevenue(const Node *listHead) {
     double totalRevenue = 0.0;
     while (listHead != NULL) {
@@ -189,7 +191,6 @@ double calculateTotalRevenue(const Node *listHead) {
     }
     return totalRevenue;
 }
-
 double calculateInvestmentInInventory(const Node *listHead) {
     double investment = 0.0;
     while (listHead != NULL) {
@@ -198,7 +199,6 @@ double calculateInvestmentInInventory(const Node *listHead) {
     }
     return investment;
 }
-
 double calculateTotalWholesaleCost(const Node *listHead) {
     double totalWholesaleCost = 0.0;
     while (listHead != NULL) {
@@ -207,14 +207,12 @@ double calculateTotalWholesaleCost(const Node *listHead) {
     }
     return totalWholesaleCost;
 }
-
 double calculateTotalProfit(const Node *listHead) {
     double totalRevenue = calculateTotalRevenue(listHead);
     double totalWholesaleCost = calculateTotalWholesaleCost(listHead);
     double investmentInInventory = calculateInvestmentInInventory(listHead);
     return (totalRevenue - totalWholesaleCost) + investmentInInventory;
 }
-
 int calculateTotalBooksSold(const Node *listHead) {
     int totalBooksSold = 0;
     while (listHead != NULL) {
@@ -223,7 +221,6 @@ int calculateTotalBooksSold(const Node *listHead) {
     }
     return totalBooksSold;
 }
-
 double calculateAverageProfit(const Node *listHead) {
     double totalProfit = calculateTotalProfit(listHead);
     int totalSales = calculateTotalBooksSold(listHead);
@@ -232,25 +229,22 @@ double calculateAverageProfit(const Node *listHead) {
     }
     return totalProfit / totalSales;
 }
-
-
 void printList(const Node *listHead) {
     const Node *traversePtr = listHead;
     printf("\nBook list:\n");
-    while (traversePtr != NULL) {
+    while (traversePtr != NULL) { /* determine not at end of list */
         printf("%s\n", traversePtr->book.title);
         traversePtr = traversePtr->next;
     }
     printf("\n");
 }
-
 void freeAllNodes(Node **listHeadPtr) {
     Node *traversePtr = *listHeadPtr;
     Node *restOfListPtr = *listHeadPtr;
-    while (restOfListPtr != NULL) {
+    while (restOfListPtr != NULL) { /* determine list is not empty */
         restOfListPtr = restOfListPtr->next;
         free(traversePtr);
         traversePtr = restOfListPtr;
     }
-    *listHeadPtr = NULL; // Set list head pointer back to NULL after freeing space
+    *listHeadPtr = NULL; /* set listHeadPtr back to NULL after space freed */
 }
